ca13365814cd6a05f7709eee42e27c56f7d6c107
LionelKarlen <lionel.karlen@protonmail.com>
2021-11-15 13:01:12 +0100

    Add game summary

--- index.ts
+++ index.ts
@@ -1,38 +1,90 @@
-import { Rummy, Meld, MeldMove, LayMove } from "rummy-lib/lib";
+import { Rummy, MeldMove, LayMove } from "rummy-lib/lib";
 import { Algo } from "./src/algo";
-import { onlyStockPickup } from "./src/pickup";
-import { minimumPossibleMeld } from "./src/meld";
-import { minimalPossibleLayoff } from "./src/layoff";
-import { oldestCard } from "./src/putdown";
+import { AutoAlgo } from "./src/autoalgo";
+import { pickups } from "./src/pickup";
+import { melds } from "./src/meld";
+import { layoffs } from "./src/layoff";
+import { putdowns } from "./src/putdown";
+import { Utils } from './src/utils';
 
-let rummy = new Rummy();
-
-let algo = new Algo(rummy, 0);
-
-while (!rummy.isGameOver) {
-	rummy.makeMove(
-		rummy.players[algo.playerIndex],
-		algo.pickUp(() => onlyStockPickup(algo.playerIndex, rummy))
-	);
-	let meld = minimumPossibleMeld(algo.playerIndex, rummy);
-	if (meld != null) {
-		rummy.makeMove(
-			rummy.players[algo.playerIndex],
-			algo.meld(() => meld as unknown as MeldMove)
-		);
+for (let f_1 = 0; f_1 < pickups.length; f_1++) {
+	for (let s_1 = 0; s_1 < melds.length; s_1++) {
+		for (let t_1 = 0; t_1 < layoffs.length; t_1++) {
+			for (let fo_1 = 0; fo_1 < putdowns.length; fo_1++) {
+				let algoNum_1 = [f_1, s_1, t_1, fo_1];
+				for (let f_2 = 0; f_2 < pickups.length; f_2++) {
+					for (let s_2 = 0; s_2 < melds.length; s_2++) {
+						for (let t_2 = 0; t_2 < layoffs.length; t_2++) {
+							for (let fo_2 = 0; fo_2 < putdowns.length; fo_2++) {
+								let algoNum_2 = [f_2, s_2, t_2, fo_2];
+								runSimulation(algoNum_1, algoNum_2);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	let layoff = minimalPossibleLayoff(algo.playerIndex, rummy);
-	if (layoff != null) {
-		rummy.makeMove(
-			rummy.players[algo.playerIndex],
-			algo.layOff(() => layoff as unknown as LayMove)
-		);
-	}
+}
 
-	let putdown = oldestCard(algo.playerIndex, rummy);
-	rummy.makeMove(
-		rummy.players[algo.playerIndex],
-		algo.putDown(() => putdown)
-	);
-	console.log(rummy.isGameOver);
+function runSimulation(algonum_1: number[], algonum_2: number[]) {
+	let rummy = new Rummy();
+
+	let algo = new Algo(rummy, 0);
+	let autoalgo = new AutoAlgo(algo, [0, 0, 0, 0]);
+
+	let secondalgo = new Algo(rummy, 1);
+
+	let secondautoalgo = new AutoAlgo(secondalgo, [0, 0, 0, 0]);
+	while (!rummy.isGameOver) {
+		rummy.makeMove(
+			rummy.players[autoalgo.algo.playerIndex],
+			autoalgo.pickUp()
+		);
+		let meld = autoalgo.meld();
+		if (meld != null) {
+			rummy.makeMove(
+				rummy.players[autoalgo.algo.playerIndex],
+				meld as unknown as MeldMove
+			);
+		}
+		let layoff = autoalgo.layoff();
+		if (layoff != null) {
+			rummy.makeMove(
+				rummy.players[autoalgo.algo.playerIndex],
+				layoff as unknown as LayMove
+			);
+		}
+
+		let putdown = autoalgo.putDown();
+		rummy.makeMove(rummy.players[autoalgo.algo.playerIndex], putdown);
+
+		rummy.makeMove(
+			rummy.players[secondautoalgo.algo.playerIndex],
+			secondautoalgo.pickUp()
+		);
+		let secondmeld = secondautoalgo.meld();
+		if (secondmeld != null) {
+			rummy.makeMove(
+				rummy.players[secondautoalgo.algo.playerIndex],
+				secondmeld as unknown as MeldMove
+			);
+		}
+		let secondlayoff = secondautoalgo.layoff();
+		if (secondlayoff != null) {
+			rummy.makeMove(
+				rummy.players[secondautoalgo.algo.playerIndex],
+				secondlayoff as unknown as LayMove
+			);
+		}
+
+		rummy.makeMove(
+			rummy.players[secondautoalgo.algo.playerIndex],
+			secondautoalgo.putDown()
+		);
+
+		console.log(rummy.isGameOver);
+		console.log(`Winner: ${rummy.players.sort((a,b)=>a.hand.cards.length-b.hand.cards.length)[0]}`);
+	}
+	Utils.summariseGame(autoalgo, secondautoalgo, rummy);
 }
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import { Rummy } from "rummy-lib/lib";
+import { AutoAlgo } from "./autoalgo";
+export class Utils {
+	static summariseGame(
+		autoAlgo_1: AutoAlgo,
+		autoAlgo_2: AutoAlgo,
+		rummy: Rummy
+	) {
+		let algoString_1 = autoAlgo_1.algoNum.join("");
+		let algoString_2 = autoAlgo_2.algoNum.join("");
+		let date = Date.now();
+		let string = `
+		SUMMARY OF GAME
+		===============
+		FINISHED: ${date}
+		FIRST: ${algoString_1} ${rummy.players[autoAlgo_1.algo.playerIndex].score}
+		SECOND: ${algoString_2} ${rummy.players[autoAlgo_2.algo.playerIndex].score}
+		WINNER: ${rummy.players.indexOf(
+			rummy.players.sort(
+				(a, b) => a.hand.cards.length - b.hand.cards.length
+			)[0]
+		)}
+		===============`;
+		console.log(string);
+		fs.writeFileSync(
+			`export/${date}-${algoString_1}-${algoString_2}.rmy`,
+			string
+		);
+	}
+}
