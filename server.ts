71c2a5e6335a85ef8706c2b7c4684e4775b8b402
LionelKarlen <lionel.karlen@protonmail.com>
2022-02-04 00:35:41 +0100

    update

--- server.ts
+++ server.ts
@@ -16,6 +16,7 @@ let io = require("socket.io")(http, {
 		origin: "http://localhost:3000",
 	},
 });
+let paused = false;
 
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
@@ -25,10 +26,31 @@ io.on("connection", function (socket: Socket) {
 	socket.on("message", function (message: any) {
 		console.log(message);
 	});
+	socket.on("pause", () => {
+		paused = !paused;
+		console.log("pause");
+	});
 	socket.on("start", async function (message) {
+		let delay = message.delay;
+		let algoNum_1 = [2, 1, 1, 1];
+		let algoNum_2 = [2, 1, 1, 1];
+		switch (message.mode) {
+			case 0:
+				algoNum_1 = [2, 1, 1, 1];
+				algoNum_2 = [2, 1, 1, 1];
+				break;
+			case 1:
+				algoNum_1 = [2, 1, 1, 1];
+				algoNum_2 = [2, 0, 0, 0];
+				break;
+			case 2:
+				algoNum_1 = [2, 0, 0, 0];
+				algoNum_2 = [2, 0, 0, 0];
+				break;
+		}
 		try {
-			let algoNum_1 = [2, 1, 1, 1];
-			let algoNum_2 = [2, 1, 1, 1];
+			// let algoNum_1 = [2, 1, 1, 1];
+			// let algoNum_2 = [2, 1, 1, 1];
 			let rummy = new Rummy();
 			socket.emit("update", rummy);
 
@@ -64,7 +86,10 @@ io.on("connection", function (socket: Socket) {
 					putdown
 				);
 				socket.emit("update", rummy);
-				await sleep(4000);
+				await sleep(delay * 1000);
+				while (paused) {
+					await sleep(1000);
+				}
 
 				rummy.makeMove(
 					rummy.players[secondautoalgo.algo.playerIndex],
@@ -90,9 +115,12 @@ io.on("connection", function (socket: Socket) {
 					secondautoalgo.putDown()
 				);
 				socket.emit("update", rummy);
-				await sleep(4000);
+				await sleep(delay * 1000);
+				while (paused) {
+					await sleep(1000);
+				}
 			}
-			let winner = rummy.players[0].hand.cards.length==0?0:1;
+			let winner = rummy.players[0].hand.cards.length == 0 ? 0 : 1;
 			socket.emit("won", winner);
 		} catch (error) {
 			socket.emit("error");
@@ -100,9 +128,9 @@ io.on("connection", function (socket: Socket) {
 	});
 });
 
-function sleep(millis:number) {
-	return new Promise(resolve => setTimeout(resolve, millis));
-  }
+function sleep(millis: number) {
+	return new Promise((resolve) => setTimeout(resolve, millis));
+}
 
 const server = http.listen(3001, function () {
 	console.log("listening on *:3001");
